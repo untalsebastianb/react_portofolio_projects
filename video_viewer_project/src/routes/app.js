660114516a195e3771b907c898f4853d19c6c717
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-03 20:09:48 -0500

    Add register container to main App

--- video_viewer_project/src/routes/app.js
+++ video_viewer_project/src/routes/app.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route} from 'react-router-dom'
 import Home from '../container/Home'
 import Login from '../container/Login'
+import Register from '../container/Register'
 
 const App = () => (
     <BrowserRouter>
     <Route exact path='/' component={Home} />
     <Route exact path='/login' component={Login} />
+    <Route exact path='/register' component={Register} />
     </BrowserRouter>
 )
 
