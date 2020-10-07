843e4169e047e3ecf984df947395df9c15794e37
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-07 16:27:39 -0500

    Add layout to the app 🎨

--- video_viewer_project/src/routes/app.js
+++ video_viewer_project/src/routes/app.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import Layout from '../components/Layout'
 import Home from '../container/Home'
 import Login from '../container/Login'
 import NotFound from '../container/NotFound'
 import Register from '../container/Register'
 
+
 const App = () => (
     <BrowserRouter>
-    <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-        <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route component={NotFound} />
+      </Switch>
+    </Layout>
     </BrowserRouter>
 )
 
