e69bc2fba11c42d1c4ae650b4c531b4bb8691016
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-07 15:42:31 -0500

    Add switch to routes

--- video_viewer_project/src/routes/app.js
+++ video_viewer_project/src/routes/app.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from '../container/Home'
 import Login from '../container/Login'
 import Register from '../container/Register'
 
 const App = () => (
-    <BrowserRouter>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/login' component={Login} />
-    <Route exact path='/register' component={Register} />
-    </BrowserRouter>
+    <Switch>
+        <BrowserRouter>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        </BrowserRouter>
+    </Switch>
 )
 
 export default App
