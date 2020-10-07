0eddfcfd6e6eebb978112652edee707e048eba0f
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-07 16:02:29 -0500

    Using Switch

--- video_viewer_project/src/routes/app.js
+++ video_viewer_project/src/routes/app.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from '../container/Home'
 import Login from '../container/Login'
+import NotFound from '../container/NotFound'
 import Register from '../container/Register'
 
 const App = () => (
+    <BrowserRouter>
     <Switch>
-        <BrowserRouter>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
-        </BrowserRouter>
+        <Route component={NotFound} />
     </Switch>
+    </BrowserRouter>
 )
 
 export default App
