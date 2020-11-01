d73d23ce818210794206b1c6b2ec7f0b9093aed1
untalsebastianb <juan.bueno01@usc.edu.co>
2020-10-31 20:09:19 -0500

    create first badge component

---
+++ badges-project/src/components/Badge.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logoConf from '../images/badge-header.svg'
+
+import './styles/Badge.css'
+import myAvatar from './styles/sebastiann.png'
+
+class Badge extends React.Component {
+  render() {
+
+    // this.props = {
+
+    // }
+
+    return (
+      <div className='Badge'>
+        <div className="Badge__header">
+          <img src={logoConf} alt='logo de la conferencia' />
+        </div>
+
+        <div className="Badge__section-name ">
+          <img className="Badge__avatar" src={myAvatar} alt="avatar" />
+          <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
+        </div>
+
+        <div className='Badge__section-info'>
+          <h3>Frontend Engineer</h3>
+          <div>@untalsebastianb</div>
+        </div>
+
+        <div className='Badge__footer'>
+          <p>Platzi conf</p>
+        </div>
+
+      </div>
+    )
+  }
+
+}
+
+export default Badge
