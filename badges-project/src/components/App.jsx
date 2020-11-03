e8eca3ff4353313f06bf2cc07e8710e7d9516b6d
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-03 18:03:11 -0500

    Add home component

--- badges-project/src/components/App.jsx
+++ badges-project/src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Badges from '../pages/Badges.jsx'
 import BadgeNew from '../pages/BadgeNew.jsx'
 import Layout from './Layout.jsx'
+import Home from '../pages/Home.jsx'
 
 export class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export class App extends Component {
           <Switch>
             <Route exact path='/badges' component={Badges} />
             <Route exact path='/badges/new' component={BadgeNew} />
+            <Route exact path='/' component={Home}/>
           </Switch>
         </Layout>
       </BrowserRouter>
