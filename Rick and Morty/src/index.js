55ec9ac5f461e68186e8aae2207f65c4e5471a99
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-04 20:10:09 -0500

    create Index js

---
+++ Rick and Morty/src/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import logo from "./images/logo.png";
+
+function CharacterCard(props) {
+  const { character } = props;
+
+  return (
+    <div
+      className="CharacterCard"
+      style={{ backgroundImage: `url(${character.image})` }}
+    >
+      <div className="CharacterCard__name-container text-truncate">
+        {character.name}
+      </div>
+    </div>
+  );
+}
+
+class App extends React.Component {
+ 
+
+  render() {
+    if (this.state.error) {
+      return "Error!";
+    }
+
+    return (
+      <div className="container">
+        <div className="App">
+          <img className="Logo" src={logo} alt="Rick y Morty" />
+
+          <ul className="row">
+            {this.state.data.results.map(character => (
+              <li className="col-6 col-md-3" key={character.id}>
+                <CharacterCard character={character} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
