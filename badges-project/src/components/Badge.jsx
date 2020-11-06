83d4d24d9dc13cc2a88d85bf3958912f9c455a0d
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-06 16:10:26 -0500

    Implement gravatar to load the photo

--- badges-project/src/components/Badge.jsx
+++ badges-project/src/components/Badge.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logoConf from '../images/badge-header.svg'
+import Gravatar from './Gravatar'
 import './styles/Badge.css'
 import myAvatar from './styles/sebastiann.png'
 
@@ -13,13 +14,18 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name ">
-          <img className="Badge__avatar" src={myAvatar} alt="avatar" />
+          {/* <img className="Badge__avatar" src={myAvatar} alt="avatar" /> */}
+          <Gravatar
+          className="Badge__avatar"
+          email={this.props.email}
+          alt='Avatar'
+          />
           <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
         </div>
 
         <div className='Badge__section-info'>
           <h3>{this.props.jobTitle}</h3>
-          <div>@{this.props.Twitter}</div>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className='Badge__footer'>
