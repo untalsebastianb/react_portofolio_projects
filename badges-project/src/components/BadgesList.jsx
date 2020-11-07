ba065f3918e5a838cd0385ade8d6dfbf8104ef18
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-07 16:37:55 -0500

    Create link to each component

--- badges-project/src/components/BadgesList.jsx
+++ badges-project/src/components/BadgesList.jsx
@@ -24,19 +24,20 @@ export class BadgesList extends Component {
       <div className='BadgesList'>
         <ul className="list-unstyled" >
           {this.props.badges.map((badge) => (
-            <li key={badge.id} className='BadgesListItem'>
-              <Gravatar
-                className='BadgesListItem__avatar'
-                email={badge.email}
-              />
-              <div className='Badge__info__container'>
-                <section className='Badge__info'>{badge.firstName} {badge.lastName}</section>
-                <span className='Twitter__logo ' />
-                <span className='Twitter__info'>@{badge.twitter}</span>
-                <section>{badge.jobTitle}</section>
-              </div>
-
-            </li>
+            <Link className='text-reset text-decoration-none' to={`/badges/${badge.id}/edit`}>
+              <li key={badge.id} className='BadgesListItem'>
+                <Gravatar
+                  className='BadgesListItem__avatar'
+                  email={badge.email}
+                />
+                <div className='Badge__info__container'>
+                  <section className='Badge__info'>{badge.firstName} {badge.lastName}</section>
+                  <span className='Twitter__logo ' />
+                  <span className='Twitter__info'>@{badge.twitter}</span>
+                  <section>{badge.jobTitle}</section>
+                </div>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
