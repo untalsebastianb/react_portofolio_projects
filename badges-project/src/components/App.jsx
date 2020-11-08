ed8d414fa779e964381190fd0d7a8995ec613588
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-08 12:21:07 -0500

    Create badge edit route

--- badges-project/src/components/App.jsx
+++ badges-project/src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Badges from '../pages/Badges.jsx'
 import BadgeNew from '../pages/BadgeNew.jsx'
 import BadgeEdit from '../pages/BadgeEdit.jsx'
+import BadgeDetails from '../pages/BadgeDetails.jsx'
 import Layout from './Layout.jsx'
 import Home from '../pages/Home.jsx'
 
@@ -15,6 +16,7 @@ export class App extends Component {
             <Route exact path='/badges' component={Badges} />
             <Route exact path='/badges/new' component={BadgeNew} />
             <Route exact path='/badges/:badgeId/edit' component={BadgeEdit} />
+            <Route exact path='/badges/:badgeId/' component={BadgeDetails} />
             <Route exact path='/' component={Home}/>
           </Switch>
         </Layout>
