81b2e4686c1b731aae52b40b80ef08416e188aa6
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-08 20:28:42 -0500

    Create Modal component

--- badges-project/src/components/Modal.jsx
+++ badges-project/src/components/Modal.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import './styles/Modal.css'
 
 
 
 export class Modal extends Component {
+
+  state = {
+    isOpen: undefined
+  }
+
   render() {
-    if(!this.props.isOpen){
+    if (!this.props.isOpen) {
       return null
     }
 
     return (
       ReactDOM.createPortal(
-        <div className='modal'></div>,
+        <div className='Modal'>
+          <div className="Modal__container">
+            <button onClick={this.props.onClose} className='Modal__close-button'>X</button>
+            {this.props.children}
+          </div>
+        </div>,
         document.getElementById('portal')
       )
     )
