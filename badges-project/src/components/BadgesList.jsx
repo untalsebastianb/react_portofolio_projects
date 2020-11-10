afcfcb6f645bb78528ce9afb2f24f3c3d4c028eb
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-10 09:44:06 -0500

    Create search filter bar using useState

--- badges-project/src/components/BadgesList.jsx
+++ badges-project/src/components/BadgesList.jsx
@@ -1,48 +1,77 @@
-import React, { Component } from 'react'
-import Badge from './Badge'
+import React, { useState } from 'react'
 import './styles/BadgesList.css'
 import { Link } from 'react-router-dom'
 import Gravatar from './Gravatar'
 
+const BadgesList = (props) => {
 
-export class BadgesList extends Component {
-  render() {
-    if (this.props.badges.length === 0) {
-      return (
-        <>
-          <h3>No badges were found</h3>
-          <Link to='/badges/new' >
-            <span className="btn btn-primary">
-              Create new badge
-        </span>
-          </Link>
-        </>
-      )
-    }
+  //  const { badges } = props
+  // This two are equivalent 
+  const badges = props.badges
 
+  const [query, setQuery] = useState('')
+
+  const filteredBadges = badges.filter(badge => {
+    return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase())
+  })
+
+  if (filteredBadges.length === 0) {
     return (
-      <div className='BadgesList'>
-        <ul className="list-unstyled" >
-          {this.props.badges.map((badge) => (
-            <Link className='text-reset text-decoration-none' to={`/badges/${badge.id}`}>
-              <li key={badge.id} className='BadgesListItem'>
-                <Gravatar
-                  className='BadgesListItem__avatar'
-                  email={badge.email}
-                />
-                <div className='Badge__info__container'>
-                  <section className='Badge__info'>{badge.firstName} {badge.lastName}</section>
-                  <span className='Twitter__logo ' />
-                  <span className='Twitter__info'>@{badge.twitter}</span>
-                  <section>{badge.jobTitle}</section>
-                </div>
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </div>
+      <>
+        <div className="form-group">
+          <label> Filter Badges</label>
+          <input type="text" className='form-control'
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value)
+              console.log(e.target.value)
+            }}
+          />
+        </div>
+
+        <h3>No badges were found</h3>
+        <Link to='/badges/new' >
+          <span className="btn btn-primary">
+            Create new badge
+        </span>
+        </Link>
+      </>
     )
   }
+
+  return (
+    <div className='BadgesList'>
+      <div className="form-group">
+        <label> Filter Badges</label>
+        <input type="text" className='form-control'
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value)
+            console.log(e.target.value)
+          }}
+        />
+      </div>
+
+      <ul className="list-unstyled" >
+        {filteredBadges.map((badge) => (
+          <Link className='text-reset text-decoration-none' to={`/badges/${badge.id}`}>
+            <li key={badge.id} className='BadgesListItem'>
+              <Gravatar
+                className='BadgesListItem__avatar'
+                email={badge.email}
+              />
+              <div className='Badge__info__container'>
+                <section className='Badge__info'>{badge.firstName} {badge.lastName}</section>
+                <span className='Twitter__logo ' />
+                <span className='Twitter__info'>@{badge.twitter}</span>
+                <section>{badge.jobTitle}</section>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default BadgesList
