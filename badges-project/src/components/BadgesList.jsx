6170c59c411929f36e4309e8edc6683f9cf7f9d0
untalsebastianb <juan.bueno01@usc.edu.co>
2020-11-10 13:32:50 -0500

    Create custom hook

--- badges-project/src/components/BadgesList.jsx
+++ badges-project/src/components/BadgesList.jsx
@@ -1,35 +1,33 @@
-import React, { useState, useMemo } from 'react'
-import './styles/BadgesList.css'
-import { Link } from 'react-router-dom'
-import Gravatar from './Gravatar'
+import React, { useState, useMemo } from "react";
+import "./styles/BadgesList.css";
+import { Link } from "react-router-dom";
+import Gravatar from "./Gravatar";
 
 // Custom hook ⚛️
 function useSearchBadges(badges) {
-  const [query, setQuery] = useState('')
-  const [filteredBadges, setFilteredResults] = useState(badges)
+  const [query, setQuery] = useState("");
+  const [filteredBadges, setFilteredResults] = useState(badges);
 
   useMemo(() => {
-    const result = badges.filter(badge => {
+    const result = badges.filter((badge) => {
       return `${badge.firstName} ${badge.lastName}`
         .toLowerCase()
-        .includes(query.toLowerCase())
-    })
+        .includes(query.toLowerCase());
+    });
 
-    setFilteredResults(result)
+    setFilteredResults(result);
+  }, [badges, query]);
 
-  }, [badges, query])
-
-  return { query , setQuery , filteredBadges}  
+  return { query, setQuery, filteredBadges };
 }
 
 const BadgesList = (props) => {
-
   //  const { badges } = props
-  // This two are equivalent 
-  const badges = props.badges
+  // This two are equivalent
+  const badges = props.badges;
 
-  const { query , setQuery, filteredBadges} = useSearchBadges(badges)
-  
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
   // Before create custom hook ✅
   // const [query, setQuery] = useState('')
   // const [filteredBadges, setFilteredResults] = useState(badges)
@@ -47,59 +45,65 @@ const BadgesList = (props) => {
 
   // Filtering with no hooks needed. ✅
   // const filteredBadges = badges.filter(badge => {
-  //   return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase())	
+  //   return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase())
   // })
-
 
   if (filteredBadges.length === 0) {
     return (
       <>
         <div className="form-group">
           <label> Filter Badges</label>
-          <input type="text" className='form-control'
+          <input
+            type="text"
+            className="form-control"
             value={query}
             onChange={(e) => {
-              setQuery(e.target.value)
-              console.log(e.target.value)
+              setQuery(e.target.value);
+              console.log(e.target.value);
             }}
           />
         </div>
 
         <h3>No badges were found</h3>
-        <Link to='/badges/new' >
-          <span className="btn btn-primary">
-            Create new badge
-        </span>
+        <Link to="/badges/new">
+          <span className="btn btn-primary">Create new badge</span>
         </Link>
       </>
-    )
+    );
   }
 
   return (
-    <div className='BadgesList'>
+    <div className="BadgesList">
       <div className="form-group">
         <label> Filter Badges</label>
-        <input type="text" className='form-control'
+        <input
+          type="text"
+          className="form-control"
           value={query}
           onChange={(e) => {
-            setQuery(e.target.value)
-            console.log(e.target.value)
+            setQuery(e.target.value);
+            console.log(e.target.value);
           }}
         />
       </div>
 
-      <ul className="list-unstyled" >
+      <ul className="list-unstyled">
         {filteredBadges.map((badge) => (
-          <Link className='text-reset text-decoration-none' to={`/badges/${badge.id}`}>
-            <li key={badge.id} className='BadgesListItem'>
+          <Link
+            className="text-reset text-decoration-none"
+            to={`/badges/${badge.id}`}
+          >
+            <li key={badge.id} className="BadgesListItem">
               <Gravatar
-                className='BadgesListItem__avatar'
+                className="BadgesListItem__avatar"
                 email={badge.email}
               />
-              <div className='Badge__info__container'>
-                <section className='Badge__info'>{badge.firstName} {badge.lastName}</section>
-                <span className='Twitter__logo ' />
-                <span className='Twitter__info'>@{badge.twitter}</span>
+              <div className="Badge__info__container">
+                <section className="Badge__info">
+                  {badge.firstName} {badge.lastName}
+                </section>
+                <span className="Twitter__logo " />
+                <span className="Twitter__info">@{badge.twitter}</span>
                 <section>{badge.jobTitle}</section>
               </div>
             </li>
@@ -107,7 +111,7 @@ const BadgesList = (props) => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default BadgesList
+export default BadgesList;
